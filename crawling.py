1751c14da9acee0323e9a7fc2c0796ecf4cb952a
Kook Hye Gyeong <locker50@naver.com>
2022-07-06 22:25:53 +0900

    변수 이름 변경, 닌텐도 뉴스 코드 작성중

--- crawling.py
+++ crawling.py
@@ -1,13 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://pokemonkorea.co.kr/")
-bsObject = BeautifulSoup(html, "lxml")
+pokemonHtml = urlopen("https://pokemonkorea.co.kr/")
+pokemonBs = BeautifulSoup(pokemonHtml, "lxml")
 
-news = bsObject.select(
+pokemonNews = pokemonBs.select(
     '#ct > section.sect-news.bg-pattern.grid2 > div > ul > li')
 
-for item in news:
+for item in pokemonNews:
     link = item.select_one(
         "#ct > section.sect-news.bg-pattern.grid2 > div > ul > li > a")["href"]
     if (link[0:5] == '/news'):
@@ -15,3 +15,16 @@ for item in news:
     elif (link[0:17] == '/pokemoncaferemix'):
         link = 'https://pokemonkorea.co.kr/'+link
     print(link)
+
+
+nintendoHtml = urlopen("https://www.nintendo.co.kr/news/list.php")
+nintendoBs = BeautifulSoup(nintendoHtml, "lxml")
+
+nintendoNews = nintendoBs.select(
+    'body > section.contents.news_page > div.news_list.article > div > div')
+
+for item in nintendoNews:
+    link = item.select_one(
+        "body > section.contents.news_page > div.news_list.article > div > div > a")["href"]
+    #어렵네요 닌텐도 웹페이지는...
+    
