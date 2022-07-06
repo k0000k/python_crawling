a9013be5357f8cbeef1d951560262f51be786c1b
Kook Hye Gyeong <locker50@naver.com>
2022-07-06 21:50:26 +0900

    url 완전하게 저장되도록 추가

--- crawling.py
+++ crawling.py
@@ -2,10 +2,16 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("https://pokemonkorea.co.kr/")
-bsObject = BeautifulSoup(html, "html.parser")
+bsObject = BeautifulSoup(html, "lxml")
 
-news = bsObject.select('#ct > section.sect-news.bg-pattern.grid2 > div > ul > li')
+news = bsObject.select(
+    '#ct > section.sect-news.bg-pattern.grid2 > div > ul > li')
 
 for item in news:
-    link=item.select_one("#ct > section.sect-news.bg-pattern.grid2 > div > ul > li > a")["href"]
+    link = item.select_one(
+        "#ct > section.sect-news.bg-pattern.grid2 > div > ul > li > a")["href"]
+    if (link[0:5] == '/news'):
+        link = 'https://www.pokemonkorea.co.kr'+link
+    elif (link[0:17] == '/pokemoncaferemix'):
+        link = 'https://pokemonkorea.co.kr/'+link
     print(link)
